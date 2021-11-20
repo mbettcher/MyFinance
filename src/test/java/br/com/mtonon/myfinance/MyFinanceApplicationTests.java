e41395e8e572fc89c734fe06c8157d2097d6cd83
mbettcher <mtonon.pmg@gmail.com>
2021-11-20 11:05:59 -0300

    Projeto Criado

---
+++ src/test/java/br/com/mtonon/myfinance/MyFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mtonon.myfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
